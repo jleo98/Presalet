9cadb00fb19786169f455b11fe93c72adae15c89
MartelAxe <martelaxe@gmail.com>
2022-11-18 20:39:16 -0600

    fixing correct addresses

--- src/contracts/addresses.js
+++ src/contracts/addresses.js
@@ -5,7 +5,7 @@ const addresses = {
     goerli: "0x8D9a2BFd40B529Ca5F0a60b6606aec376554add0",
     bsctestnet: "0x58C388bb71Ff2a926963cf888Cd793De700d6E9e",
     mumbai: "0x5f4395996479eF6ff57da6f28779Cc8EA2350258",
-    bsc: "0x5AE6862B92Fe443D2C4addD9C6e65Fc0C7ccdDc0"
+    bsc: "0xa0139aa6994E9126Fa108863d0f314d970306F46"
   },
   goldList: {
     goerli: "0xf68a7fea591bfa6d0e912d9acb0278a7d2cd928d",
@@ -17,7 +17,7 @@ const addresses = {
     goerli: "0x65c307AB0d8a745C8Ba2D81a3436dE16bb4c4050",
     bsctestnet: "0x3D2A57136B2458a175a7eE3C4781dBa5750659D0",
     mumbai: "0xb002d04aee07eF870231D2Ef0CBdD4f936CF0D4E",
-    bsc: "0xa0139aa6994E9126Fa108863d0f314d970306F46"
+    bsc: "0x577B2EecadF6D8cA93237C849d1AF0D0bB0919B1"
   }
 };
 
