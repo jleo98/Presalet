34fbdac09660707289131b3d2ce3d49f7b7d66fe
MartelAxe <martelaxe@gmail.com>
2022-11-18 20:48:54 -0600

    Fixing addresses again...

--- src/contracts/addresses.js
+++ src/contracts/addresses.js
@@ -5,13 +5,13 @@ const addresses = {
     goerli: "0x8D9a2BFd40B529Ca5F0a60b6606aec376554add0",
     bsctestnet: "0x58C388bb71Ff2a926963cf888Cd793De700d6E9e",
     mumbai: "0x5f4395996479eF6ff57da6f28779Cc8EA2350258",
-    bsc: "0xa0139aa6994E9126Fa108863d0f314d970306F46"
+    bsc: "0x5AE6862B92Fe443D2C4addD9C6e65Fc0C7ccdDc0"
   },
   goldList: {
     goerli: "0xf68a7fea591bfa6d0e912d9acb0278a7d2cd928d",
     bsctestnet: "0x5c19ec2dfd13cbdc5efa34f77f4bc83669604fa6",
     mumbai: "0x1390746a374ED3c7e38eb5D099ef9D6Ed4cA457C",
-    bsc: "0x38F6dD59eC1269c04DF5E4906EddabCF3eD4bCA0"
+    bsc: "0xa0139aa6994E9126Fa108863d0f314d970306F46"
   },
   coldStaking: {
     goerli: "0x65c307AB0d8a745C8Ba2D81a3436dE16bb4c4050",
